86a570d25362641bdc0cdaef9628cfa54d742f05
Guido Zockoll <Guido Zockoll@payone.de>
2011-11-07 16:21:14 +0100

    added feature.xml

--- src/test/java/de/gzockoll/pdfcategorizer/AirBerlinScannerTest.java
+++ src/test/java/de/gzockoll/pdfcategorizer/AirBerlinScannerTest.java
@@ -1,6 +1,5 @@
 package de.gzockoll.pdfcategorizer;
 
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
@@ -17,43 +16,46 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class AirBerlinScannerTest {
-	private static DocumentScanner scanner;
+    private static DocumentScanner scanner;
 
-	private static final String CORRECT_MATCH="Air Berlin PLC & Co. Luftverkehrs KG\n\nRechnung und Reisebestätigung\n\nBuchungs/Rechnungsnummer 12345\n\nBuchungsdatum 26.04.2011\n\n....";
-	@Before
-	public void setUp() throws Exception {
-		scanner=new AirBerlinScanner();
-	}
-	
-	@Test
-	public void testSupports() {
-		assertFalse(scanner.supports("some random string"));
-		assertTrue(scanner.supports(CORRECT_MATCH));
-	}
-	@Test
-	public void testGetDate() throws ParseException {
-		DocumentInfo info = scanner.scan(CORRECT_MATCH);
-		assertThat(info.getDate(),is(DateFormat.getDateInstance(DateFormat.SHORT,Locale.GERMAN).parse("26.04.2011")));
-	}
+    private static final String CORRECT_MATCH = "Air Berlin PLC & Co. Luftverkehrs KG\n\nRechnung und Reisebestätigung\n\nBuchungs/Rechnungsnummer 12345\n\nBuchungsdatum 26.04.2011\n\n....";
 
-	@Test
-	public void testGetTitle() throws ParseException {
-		DocumentInfo info = scanner.scan(CORRECT_MATCH);
-		assertThat(info.getTitle(),is("AirBerlin Rechnung 12345"));
-	}
-	
-	@Test 
-	public void testWithRealData() throws IOException, ParseException {
-		PDFTextStripper stripper = new PDFTextStripper();
-		String text=null;
-		try {
-			text = stripper.getText(PDDocument.load("src/main/resources/12529529.pdf"));
-		} catch (NullPointerException e) {
-		}
-		assertTrue(scanner.supports(text));
-		DocumentInfo info = scanner.scan(text);
-		assertThat(info.getDate(),is(DateFormat.getDateInstance(DateFormat.SHORT,Locale.GERMAN).parse("26.04.2011")));
-		assertThat(info.getTitle(),is("AirBerlin Rechnung 12529529"));
-		
-	}
+    @Before
+    public void setUp() throws Exception {
+        scanner = new AirBerlinScanner();
+    }
+
+    @Test
+    public void testGetDate() throws ParseException {
+        DocumentInfo info = scanner.scan(CORRECT_MATCH);
+        assertThat(info.getDate(), is(DateFormat.getDateInstance(DateFormat.SHORT, Locale.GERMAN).parse("26.04.2011")));
+    }
+
+    @Test
+    public void testGetTitle() throws ParseException {
+        DocumentInfo info = scanner.scan(CORRECT_MATCH);
+        assertThat(info.getTitle(), is("AirBerlin Rechnung 12345"));
+    }
+
+    @Test
+    public void testSupports() {
+        assertFalse(scanner.supports("some random string"));
+        assertTrue(scanner.supports(CORRECT_MATCH));
+    }
+
+    @Test
+    public void testWithRealData() throws IOException, ParseException {
+        PDFTextStripper stripper = new PDFTextStripper();
+        String text = null;
+        try {
+            text = stripper.getText(PDDocument.load("src/main/resources/12529529.pdf"));
+        } catch (NullPointerException e) {
+            // catching some stange exceptions while loading document
+        }
+        assertTrue(scanner.supports(text));
+        DocumentInfo info = scanner.scan(text);
+        assertThat(info.getDate(), is(DateFormat.getDateInstance(DateFormat.SHORT, Locale.GERMAN).parse("26.04.2011")));
+        assertThat(info.getTitle(), is("AirBerlin Rechnung 12529529"));
+
+    }
 }
